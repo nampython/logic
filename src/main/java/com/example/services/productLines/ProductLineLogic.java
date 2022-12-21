4246b7787f79d0a3d01566da7f2b540f7b5daa7b
Jerry <jerry.nguyen@terralogic.com>
2022-12-21 16:34:35 +0700

    message = productLine logic

--- src/main/java/com/example/services/productLines/ProductLineLogic.java
+++ src/main/java/com/example/services/productLines/ProductLineLogic.java
@@ -4,4 +4,5 @@ import com.example.domain.productlinedomain.ProductLineConfig;
 
 public interface ProductLineLogic {
     public abstract ProductLineConfig getAllProductLines();
+    public abstract ProductLineConfig saveProductLines(ProductLineConfig productLineConfig);
 }
